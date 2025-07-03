bc8104743c3a396007cf6976f130536118a1eb81
Yushan Hettiarachchi <130626574+Dismi343@users.noreply.github.com>
2025-07-04 02:59:17 +0530

    hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
-import PhotoSlider from "../components/PhotoSilder";
 import { useState,useEffect } from "react";
 import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import ConferenceSpeakers from "../components/ConferenceSpeakers";
-import CountdownSection from "../components/Countdown";
 
 function Home(){
 
@@ -46,13 +44,20 @@ function Home(){
         <div className="w-full md:h-[850px] h-[350px]  flex justify-center center   " >
         <div className="md:flex w-full ">
             <div className={`w-full flex   transition-all duration-1500 ease-out delay-200  ${visible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}>
-            <div className="w-full md:h-[850px] h-[300px] text-center bg-cover bg-center bg-[url(./assets/aboutimg/Bg1.jpg)]  shadow-lg shadow-black/50 relative">
+            <div className="w-full md:h-[700px] h-[300px] text-center bg-cover bg-center bg-[url(./assets/aboutimg/Bg1.jpg)]  shadow-lg shadow-black/50 relative">
                 {/* Gradient overlay */}
                 <div className="absolute inset-0 z-10 bg-gradient-to-r from-black via-black/60 to-transparent"></div>
-                <div className="md:w-1/2 flex flex-col md:items-start pl-20 mt-10 relative z-20">
+                <div className="md:w-1/2 flex flex-col md:items-start pl-20 mt-15 relative z-20">
                     <h1 className={` font-inter font-[600] leading-none lg:text-[96px] md:text-[60px] text-[20px] text-white  mt-20 text-shadow-lg/30  transition-all duration-1500 ease-out delay-400  ${visible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}>RUHUNA</h1>
                     <h3 className={` font-inter font-[600] lg:text-[96px] md:text-[60px] leading-none text-[20px] text-[#CC27FF]  md:block  text-shadow-lg/30  transition-all duration-1500 ease-out delay-500  ${visible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}>INTERNATIONAL</h3>
                     <h3 className={` font-inter font-[600]  tracking-widest leading-none lg:text-[30px] md:text-[24px] text-[20px] text-white  md:block  text-shadow-lg/30  transition-all duration-1500 ease-out delay-500  ${visible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}>UNDERGRADUATE SCIENCE SYMPOSIUM</h3>
+                </div>
+                <div className={`flex  flex-row  mt-10 relative z-20 w-1/2 pl-20 text-shadow-lg/30  transition-all duration-1500 ease-out delay-1000  ${visible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}>
+                   <div className="  h-40  w-1/6 text-[#CC27FF] lg:text-[104px] font-inter font-[600] items-center  flex mr-12 "><p className="ordinal ">12<sup class="text-[60px] align-super">th</sup></p></div>
+                    <div className=" h-40  w-5/6 items-center   flex text-start"><p className=" text-white text-[25px] font-inter font-[600]">NOVEMBER 2025 <br/> Faculty of Science, University of Ruhuna</p></div>
+                </div>
+                <div className={`flex  flex-row  mt-6 relative z-20 w-1/2 pl-20 text-shadow-lg/30  transition-all duration-1500 ease-out delay-1500  ${visible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}>
+                            <p className="text-white text-xl">"Research for shaping the future"</p>
                 </div>
             </div>
             </div>
@@ -65,7 +70,6 @@ function Home(){
                     <p id='st-section' className={`text-center px-10 mt-15 transition-all duration-1500 ease-out delay-100 text-[15px] md:text-[20px] ${slide ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}>RUSS 2025 is organized by the Faculty of Science, University of Ruhuna. This aims to provide a premier multidisciplinary forum for undergraduate researchers to present their research findings, innovations, practical challenges encountered and the solutions adopted in the fields of Science & Technology.</p>
         </div>
         
-            <PhotoSlider/>
             <ConferenceSpeakers />
             
             <div className="w-full h-96 mt-30">
